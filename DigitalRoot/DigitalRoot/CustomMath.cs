c072517fc3c56f99fe5094352e92951e7e29b28a
Ilia Valchenko <ilia.valchenko.epam@gmail.com>
2016-10-18 21:43:32 +0300

    Add DigitalRoot kata 6 level

---
+++ DigitalRoot/DigitalRoot/CustomMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DigitalRoot
+{
+    public static class CustomMath
+    {
+        public static int DigitalRoot(long n)
+        {
+            while (n.ToString().Length >= 2)
+            {
+                long a = (long)Char.GetNumericValue(n.ToString()[0]);
+
+                for (int i = 1; i < n.ToString().Length; i++)
+                    a += (int)Char.GetNumericValue(n.ToString()[i]);
+
+                n = a;
+            }
+
+            return (int)n;
+        }
+    }
+}
