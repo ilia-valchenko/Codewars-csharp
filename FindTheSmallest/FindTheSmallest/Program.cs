162dc1212ea3bf4b423c11c3c22cb8d8614821c1
Ilia Valchenko <ilia.valchenko.epam@gmail.com>
2016-10-30 12:29:58 +0300

    Update my solution.

--- FindTheSmallest/FindTheSmallest/Program.cs
+++ FindTheSmallest/FindTheSmallest/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FindTheSmallest
 {
@@ -10,11 +7,30 @@ namespace FindTheSmallest
     {
         static void Main(string[] args)
         {
-            long n = 261235;
+            long n = 205900826587298752;
+
+            //long n = 949054622533291168;
+
+            //long n = 97326710027329400;
+
+            //long n = 935855753;
+
+            //long n = 199819884756;
+
+            //long n = 86741065181209200;
+
+            //long n = 227090135788124416;
+            //long n = 15519177548367362;
+
+            //long n = 261235;
+            //long n = 209917; 
+            //long n = 285365; 
+            //long n = 269045;
+            //long n = 296837; 
 
             var arr = Smallest(n);
 
-            Console.Write("\nResult: [ ");
+            Console.Write("\n\nResult: [ ");
             foreach (var item in arr)
                 Console.Write(item + " ");
             Console.Write("]");
@@ -27,50 +43,129 @@ namespace FindTheSmallest
         {
             var arr = GetDigits(n.ToString());
 
-            Console.Write("Get digits: ");
-            foreach (int item in arr)
-                Console.Write(item + " ");
 
-            var val = GetMinValueAndIndex(arr);
-            int min = val[0];
-            int indexOfMin = val[1];
+            //Console.Write("Get digits: ");
+            //foreach (int item in arr)
+            //    Console.Write(item + " ");
 
-            // fix it
-            if(indexOfMin == 0)
-
-            
-            Console.WriteLine($"\n\nMinimal element: {min}");
-            Console.WriteLine($"\nIndex of minimal element: {indexOfMin}");
+            var sortedArray = new int[arr.Length];
+            arr.CopyTo(sortedArray, 0);
+            Array.Sort(sortedArray);
 
             var result = new int[arr.Length];
+            int newIndexOfMin = -1;
+            int oldIndexOfMin = -1;
+            int min = sortedArray[0];
 
-            if (indexOfMin == 0)
+            //Console.Write("\n\nSorted array: ");
+            //foreach (int item in sortedArray)
+            //    Console.Write(item + " ");
+
+            if (arr[0] == sortedArray[sortedArray.Length - 1] && min != 0)
             {
-                result = arr;
+                if (arr[arr.Length - 1] == 0)
+                {
+                    for (int i = 1; i < result.Length - 1; i++)
+                        result[i] = arr[i + 1];
+
+                    result[0] = 0;
+
+                    oldIndexOfMin = arr.Length - 1;
+                    newIndexOfMin = 0;
+                }
+                else
+                {
+                    for (int i = 0; i < arr.Length - 1; i++)
+                        result[i] = arr[i + 1];
+
+                    result[result.Length - 1] = sortedArray[sortedArray.Length - 1];
+
+                    oldIndexOfMin = 0;
+                    newIndexOfMin = result.Length - 1;
+                }
             }
             else
             {
-                var helper = new int[arr.Length - 1];
-
-                for (int i = 0, j = 0; i < arr.Length; i++)
-                    if (i != indexOfMin)
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] != sortedArray[i] || arr[i] == min)
                     {
-                        helper[j] = arr[i];
-                        j++;
+                        newIndexOfMin = i;
+                        break;
+                    }
+                }
+
+                // Find old index of min
+                for (int i = 0; i < arr.Length; i++)
+                    if (arr[i] == min)
+                    {
+                        oldIndexOfMin = i;
+                        break;
                     }
 
-                result[0] = min;
+                // fix it 
+                if (min == 0 && oldIndexOfMin == 1)
+                {
+                    min = arr[0];
+                    oldIndexOfMin = 0;
+                    newIndexOfMin = 1;
 
-                for (int i = 0; i < helper.Length; i++)
-                    result[i + 1] = helper[i];
+                    arr[1] = min;
+                    arr[0] = 0;
+                    result = arr;
+                }
+                else
+                {
+                    // Find the last of a few min elements
+                    for (int i = oldIndexOfMin + 1; i < arr.Length; i++)
+                    {
+                        if (arr[i] == min)
+                            oldIndexOfMin = i;
+                    }
+
+                    // test
+                    while (arr[oldIndexOfMin] == arr[oldIndexOfMin - 1])
+                        oldIndexOfMin--;
+
+                    //Console.Write($"\n\nIndex of the last of min elements is {oldIndexOfMin}");
+
+                    //Console.Write($"\n\nMinnimal element is {min}");
+                    //Console.Write($"\n\nNew index of the minimal element is {newIndexOfMin}");
+                    //Console.Write($"\n\nOld index of min {oldIndexOfMin}");
+
+                    for (int j = 0; j < newIndexOfMin; j++)
+                        result[j] = arr[j];
+
+                    result[newIndexOfMin] = min;
+
+                    //Console.Write("\n\nFIRST FIRST part: ");
+                    //foreach (int item in result)
+                    //    Console.Write(item + " ");
+
+                    for (int i = newIndexOfMin; i < arr.Length - 1; i++)
+                    {
+                        if (i < oldIndexOfMin)
+                        {
+                            result[i + 1] = arr[i];
+                        }
+                        else
+                        {
+                            result[i + 1] = arr[i + 1];
+                        }
+                    }
+                }
             }
+
+            //Console.Write("\n\nResulting array NOW NOW NOW: ");
+            //foreach (int item in result)
+            //    Console.Write(item + " ");
 
             var str = new StringBuilder();
 
             for (int i = 0; i < result.Length; i++)
                 str.Append(result[i]);
 
-            long final = Convert.ToInt64(str.ToString()), startIndex = indexOfMin, finalIndex = 0;
+            long final = Convert.ToInt64(str.ToString()), startIndex = oldIndexOfMin, finalIndex = newIndexOfMin;
 
             return new long[] { final, startIndex, finalIndex };
         }
@@ -83,29 +178,6 @@ namespace FindTheSmallest
                 digits[i] = (int)Char.GetNumericValue(number[i]);
 
             return digits;
-        }
-
-        public static int[] GetMinValueAndIndex(int[] array)
-        {
-            var arr = new int[array.Length];
-            array.CopyTo(arr, 0);
-            Array.Sort(arr);
-
-            Console.Write("\n\nSorted array: ");
-            foreach (int item in arr)
-                Console.Write(item + " ");
-
-            int min = arr[0];
-            int indexOfMin = -1;
-
-            for (int i = 0; i < array.Length; i++)
-                if (array[i] == min)
-                {
-                    indexOfMin = i;
-                    break;
-                }
-
-            return new int[] {min, indexOfMin};
         }
     }
 }
